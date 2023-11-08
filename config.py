fa25ae140671073bc68585c1c493577c217feb26
Kirill <holmenovkirill@gmail.com>
2023-11-08 21:46:10 +0300

    Delete link

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TABLE_URL = 'https://docs.google.com/spreadsheets/d/1sTF1_vCVMDDpchvmHn6w9ClQVGjQT1dFbz1AHh14ajI/edit#gid=0'
+TABLE_URL = 'link'
 SHEET_ID = 0
 CELLS_SKINS = 'A2:A1000'
 CELLS_PRICE = 'C2:C1000'
